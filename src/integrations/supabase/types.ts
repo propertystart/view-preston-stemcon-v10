b422327059e8c5fde9f0fd2ce4d40b74bacb044f
PropertyStart VR <43327764+propertystart@users.noreply.github.com>
2025-05-15 14:11:47 +1000

    Update types.ts

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -9,7 +9,7 @@ export type Json =
 export type Database = {
   public: {
     Tables: {
-      contact_submissions: {
+      contacts_stemcon: {
         Row: {
           created_at: string
           email: string
